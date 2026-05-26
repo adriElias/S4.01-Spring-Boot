4fe54f78b6ada12dd9894c3ca79bcc9af04b4bef
adriElias <34091624+adriElias@users.noreply.github.com>
2026-05-26 11:22:34 +0200

    feat: return JSON object in HealthController

--- src/main/java/cat/itacademy/s04/t01/userapi/level1/controllers/HealthController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/level1/controllers/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    public String healthMessage(){
-        return "OK";
+    public Status healthMessage(){
+        return new Status("OK");
     }
 }
---
+++ src/main/java/cat/itacademy/s04/t01/userapi/level1/controllers/Status.java
@@ -0,0 +1,3 @@
+package cat.itacademy.s04.t01.userapi.level1.controllers;
+
+public record Status(String status) { }
