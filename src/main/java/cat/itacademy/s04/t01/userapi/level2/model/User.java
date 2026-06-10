255343f5191216cfc7139bdd91df7181056c8224
adriElias <34091624+adriElias@users.noreply.github.com>
2026-06-10 20:32:01 +0200

    feat: create user model

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/level2/model/User.java
@@ -0,0 +1,15 @@
+package cat.itacademy.s04.t01.userapi.level2.model;
+
+import java.util.UUID;
+
+public class User {
+    private final UUID id;
+    private String name;
+    private String email;
+
+    public User(UUID id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
