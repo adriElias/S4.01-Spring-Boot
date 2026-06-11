fb1666cad60ffa3af34cd24e95d91c128f61392b
adriElias <34091624+adriElias@users.noreply.github.com>
2026-06-11 03:32:12 +0200

    feat: add POST/users endpoint to create a new user with random UUID

--- src/main/java/cat/itacademy/s04/t01/userapi/level2/controller/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/level2/controller/UserController.java
@@ -1,12 +1,16 @@
 package cat.itacademy.s04.t01.userapi.level2.controller;
 
 import cat.itacademy.s04.t01.userapi.level1.Status;
+import cat.itacademy.s04.t01.userapi.level2.dto.CreateUserRequest;
 import cat.itacademy.s04.t01.userapi.level2.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -17,4 +21,11 @@ public class UserController {
         return users;
     }
 
+    @PostMapping("/users")
+    public User createUser(@RequestBody CreateUserRequest userRequest){
+        UUID id =UUID.randomUUID();
+        User user = new User(id, userRequest.name(), userRequest.email());
+        users.add(user);
+        return user;
+    }
 }
---
+++ src/main/java/cat/itacademy/s04/t01/userapi/level2/dto/CreateUserRequest.java
@@ -0,0 +1,3 @@
+package cat.itacademy.s04.t01.userapi.level2.dto;
+
+public record CreateUserRequest(String name, String email) { }
